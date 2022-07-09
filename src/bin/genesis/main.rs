3ba4a622fcc9674116feac1afcf077f599a5ab86
Ben Gale <bengale2007@googlemail.com>
2022-07-09 21:19:43 +0100

    Changed sprite

--- src/bin/genesis/main.rs
+++ src/bin/genesis/main.rs
@@ -10,7 +10,7 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("branding/icon.png"),
+        texture: asset_server.load("sprite.png"),
         ..default()
     });
 }
