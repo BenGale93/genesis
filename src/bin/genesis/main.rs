661e3c19aedc61802db1954580c12c54234f6443
Ben Gale <bengale2007@googlemail.com>
2022-10-22 16:45:36 +0100

    Moved Genesis set up into a plugin

--- src/bin/genesis/main.rs
+++ src/bin/genesis/main.rs
@@ -1,33 +1,17 @@
-use bevy::prelude::*;
+use bevy::{prelude::App, window::WindowDescriptor, DefaultPlugins};
 use bevy_egui::EguiPlugin;
-use bevy_rapier2d::prelude::*;
-use genesis::{config, resources, setup, systems, ui};
+use bevy_rapier2d::prelude::{NoUserData, RapierPhysicsPlugin};
+use genesis::GenesisPlugin;
 
 fn main() {
-    config::initialize_config();
-
     App::new()
+        .add_plugin(GenesisPlugin)
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1000.0))
-        .insert_resource(config::BACKGROUND)
-        .insert_resource(ui::PanelState::default())
         .insert_resource(WindowDescriptor {
             title: "Genesis".to_string(),
-            ..default()
+            ..Default::default()
         })
-        .insert_resource(resources::Ecosystem::new(
-            config::WorldConfig::global().world_energy,
-        ))
-        .add_startup_system(setup::camera_setup)
-        .add_startup_system(setup::physics_setup)
-        .add_system_set(systems::interaction_system_set())
-        .add_system_set(systems::behavior_system_set())
-        .add_system_set(systems::egg_spawning_system_set())
-        .add_system_set(systems::plant_spawning_system_set())
-        .add_system_set(systems::slow_behavior_system_set())
-        .add_system_set(systems::burnt_energy_system_set())
-        .add_system_set(systems::selection_system_set())
-        .add_system(ui::energy_ui_update_system)
         .run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,40 @@
 #![allow(dead_code)]
+use bevy::prelude::{App, Plugin};
+
 mod attributes;
 mod body;
-pub mod config;
+mod config;
 mod ecosystem;
 mod interaction;
 mod lifecycle;
 mod mind;
 mod movement;
-pub mod setup;
+mod setup;
 mod sight;
 mod spawn;
-pub mod systems;
-pub mod ui;
+mod systems;
+mod ui;
 
-pub mod resources {
-    pub use crate::ecosystem::Ecosystem;
+pub struct GenesisPlugin;
+
+impl Plugin for GenesisPlugin {
+    fn build(&self, app: &mut App) {
+        config::initialize_config();
+
+        app.insert_resource(config::BACKGROUND)
+            .insert_resource(ui::PanelState::default())
+            .add_startup_system(setup::camera_setup)
+            .add_startup_system(setup::physics_setup)
+            .add_system_set(systems::interaction_system_set())
+            .add_system_set(systems::behavior_system_set())
+            .add_system_set(systems::egg_spawning_system_set())
+            .add_system_set(systems::plant_spawning_system_set())
+            .add_system_set(systems::slow_behavior_system_set())
+            .add_system_set(systems::burnt_energy_system_set())
+            .add_system_set(systems::selection_system_set())
+            .insert_resource(ecosystem::Ecosystem::new(
+                config::WorldConfig::global().world_energy,
+            ))
+            .add_system(ui::energy_ui_update_system);
+    }
 }
