9d3dcea04c642758f7562c2290f439c1081afe93
Ben Gale <bengale2007@googlemail.com>
2022-11-29 17:10:24 +0000

    Added coloring to bugs based on brains

--- src/mind.rs
+++ src/mind.rs
@@ -1,6 +1,8 @@
-use bevy::prelude::{Bundle, Component};
+use bevy::prelude::{Bundle, Color, Component};
 use derive_more::{Deref, DerefMut, From};
 use genesis_brain::Brain;
+use rand::{rngs::StdRng, SeedableRng};
+use rand_distr::{Distribution, Uniform};
 
 use crate::config;
 
@@ -16,6 +18,36 @@ impl Mind {
         }
 
         Self(brain)
+    }
+
+    pub fn color(&self) -> Color {
+        let innovations = self.0.innovations();
+        let mut first_innovation = match innovations.first() {
+            Some(i) => *i,
+            None => return Color::WHITE,
+        };
+        let mut rng = StdRng::seed_from_u64(first_innovation as u64);
+        let uniform = Uniform::new(0.0, 1.0);
+
+        let mut r = uniform.sample(&mut rng);
+        let mut g = uniform.sample(&mut rng);
+        let mut b = uniform.sample(&mut rng);
+
+        for (i, innovation) in self.innovations().iter().skip(1).enumerate() {
+            let diff = *innovation as f32 - first_innovation as f32;
+            let perturbation = 1.0 / diff;
+            let index_mod = i % 3;
+            if index_mod == 0 {
+                r = (r + perturbation).clamp(0.0, 1.0);
+            } else if index_mod == 1 {
+                g = (g + perturbation).clamp(0.0, 1.0);
+            } else {
+                b = (b + perturbation).clamp(0.0, 1.0);
+            }
+            first_innovation = *innovation;
+        }
+
+        Color::rgb(r, g, b)
     }
 }
 
