363f5b4ccce9b41a3af67a41c97feb8e967becef
Ben Gale <bengale2007@googlemail.com>
2022-12-14 21:39:35 +0000

    Relations is now made up of u32 values
    The cantor pairing function is applied so we get a unique value.
    Cantor pairing function has been moved to maths module.

--- src/ancestors.rs
+++ src/ancestors.rs
@@ -1,24 +1,27 @@
 use bevy::prelude::{Component, Entity, Resource};
+use genesis_util::maths;
 use serde_derive::Serialize;
 
 #[derive(Debug, Component, Serialize, Clone)]
 pub struct Relations {
-    entity: Entity,
-    parent: Option<Entity>,
-    children: Vec<Entity>,
+    entity: u32,
+    parent: Option<u32>,
+    children: Vec<u32>,
 }
 
 impl Relations {
-    pub const fn new(entity: Entity, parent: Option<Entity>) -> Self {
+    pub fn new(entity: Entity, parent: Option<Entity>) -> Self {
+        let parent = parent.map(|e| maths::cantor_pairing(e.generation(), e.index()));
         Self {
-            entity,
+            entity: maths::cantor_pairing(entity.generation(), entity.index()),
             parent,
             children: vec![],
         }
     }
 
     pub fn add_child(&mut self, child: Entity) {
-        self.children.push(child)
+        self.children
+            .push(maths::cantor_pairing(child.generation(), child.index()))
     }
 
     pub fn is_interesting(&self) -> bool {
