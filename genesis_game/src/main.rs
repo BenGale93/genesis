96a994c18c48a232e24c9c0a03642d4801110500
Ben Gale <bengale2007@googlemail.com>
2023-01-25 20:22:24 +0000

    Added FPS counter

--- genesis_game/src/main.rs
+++ genesis_game/src/main.rs
@@ -1,10 +1,12 @@
 use bevy::{
     prelude::{default, App, PluginGroup},
+    ui::{AlignSelf, PositionType, Style, UiRect, Val},
     window::{WindowDescriptor, WindowPlugin},
     DefaultPlugins,
 };
 use bevy_egui::EguiPlugin;
 use bevy_rapier2d::prelude::*;
+use bevy_screen_diagnostics::{ScreenDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin};
 use genesis_lib::GenesisPlugin;
 
 fn main() {
@@ -23,5 +25,19 @@ fn main() {
     app.add_plugin(EguiPlugin)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1000.0))
         .add_plugin(GenesisPlugin)
+        .add_plugin(ScreenDiagnosticsPlugin {
+            style: Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    top: Val::Px(5.0),
+                    left: Val::Percent(50.0),
+                    ..default()
+                },
+                ..default()
+            },
+            ..default()
+        })
+        .add_plugin(ScreenFrameDiagnosticsPlugin)
         .run();
 }
