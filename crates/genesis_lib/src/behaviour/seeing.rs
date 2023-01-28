2a64437f54c1e6efa9e4c90afb323e53c578088d
Ben Gale <bengale2007@googlemail.com>
2023-01-28 11:30:51 +0000

    Added benchmark for sight system
    60,615,395 ns/iter (+/- 2,792,770)

--- crates/genesis_lib/src/behaviour/seeing.rs
+++ crates/genesis_lib/src/behaviour/seeing.rs
@@ -81,9 +81,60 @@ pub fn process_sight_system(
 
 #[cfg(test)]
 mod tests {
-    use bevy::prelude::{Quat, Transform};
 
-    use super::dist_angle_score;
+    use bevy::prelude::{App, Quat, Transform};
+    use genesis_attributes::Genome;
+    use genesis_config::{initialize_configs, BEHAVIOUR_TICK, INPUT_NEURONS, OUTPUT_NEURONS};
+    use iyes_loopless::prelude::*;
+    use rand::{rngs::StdRng, SeedableRng};
+    use rand_distr::{Distribution, Uniform};
+    use test::Bencher;
+
+    use super::*;
+
+    #[bench]
+    fn bench_sight_system(b: &mut Bencher) {
+        initialize_configs(None);
+        let mut rng = StdRng::seed_from_u64(2);
+        let uniform = Uniform::new(-500.0, 500.0);
+        let mut app = App::new();
+        app.init_resource::<FixedTimesteps>()
+            .add_fixed_timestep(BEHAVIOUR_TICK, "standard");
+
+        app.add_system(process_sight_system);
+
+        for _ in 0..100 {
+            let transform =
+                Transform::from_xyz(uniform.sample(&mut rng), uniform.sample(&mut rng), 0.0);
+            let plant = (transform, Plant);
+            app.world.spawn(plant);
+        }
+
+        let genome = Genome::new();
+        let mind = Mind::minimal(INPUT_NEURONS, OUTPUT_NEURONS, &[]);
+
+        for _ in 0..100 {
+            let transform =
+                Transform::from_xyz(uniform.sample(&mut rng), uniform.sample(&mut rng), 0.0);
+            let eye_range = EyeRange::new(400.0);
+            let eye_angle = EyeAngle::new(*eye_range, &genome.eye_range);
+            let bug = (
+                transform,
+                eye_range,
+                eye_angle,
+                mind.clone(),
+                Vision::new(),
+                AgeEfficiency(1.0),
+            );
+            app.world.spawn(bug);
+        }
+
+        b.iter(|| {
+            for _ in 0..100 {
+                app.update()
+            }
+        });
+    }
 
     #[test]
     fn angle_score_straight_ahead() {
